6c655682831c8bd1dc4f3ff30b3ad5cd39f3f34c
j2deme <j2deme@gmail.com>
2023-10-01 18:54:12 -0600

    Agrega router básico para el SPA

---
+++ src/router.js
@@ -0,0 +1,10 @@
+import * as VueRouter from "vue-router";
+
+const routes = [];
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes,
+});
+
+export default router;
